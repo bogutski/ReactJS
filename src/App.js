7a0a12f662f53b2479f621f93384d31d63dfa84e
artsenius <34055997+artsenius@users.noreply.github.com>
2018-03-07 20:57:32 -0800

    added current year with copyright to footer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
 
+function yearNow(){
+let today = new Date();
+  return today.getFullYear();
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <Header siteName="REACT"/>
         <Content />
-        <Footer />
+        <Footer year = {yearNow()}/>
 
       </div>
     );
--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <div>
-        -Footer-
+        © {this.props.year} React JS
       </div>
     );
   }
